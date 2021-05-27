95ba51b807b3e1838645416daae0508ee0e198a0
Jesus Lopez <a6519150014@utch.edu.mx>
2021-05-26 20:48:50 -0600

    Error 404

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Main from "./pages/main/Main"
 import NewBadge from './pages/NewBadge/NewBadge'
 import Sign_up from './components/Sign_up'
+import Log_in from './components/Log_in'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/new" component={NewBadge}></Route>
           <Route exact path="/sign_up" component={Sign_up}></Route>
+          <Route exact path="/log_in" component={Log_in}></Route>
           <Route component={Error}></Route>
         </Switch>
       </Layout>
