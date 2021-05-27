283c864c3d51902634fc2486f526328afcc8361c
Jesus Lopez <a6519150014@utch.edu.mx>
2021-05-26 21:17:24 -0600

    Error 404

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./pages/main/Main"
 import NewBadge from './pages/NewBadge/NewBadge'
 import Sign_up from './components/Sign_up'
 import Log_in from './components/Log_in'
+import None from './components/none'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/new" component={NewBadge}></Route>
           <Route exact path="/sign_up" component={Sign_up}></Route>
           <Route exact path="/log_in" component={Log_in}></Route>
+          <Route exact path="/none" component={None}></Route>
           <Route component={Error}></Route>
         </Switch>
       </Layout>
---
+++ src/components/none.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./style/none.css"
+
+class None extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <center>
+                    <img src="https://www.seoinhouse.es/wp-content/uploads/2017/12/pagina-error-404.jpg" className="none"/>
+                </center>
+            </React.Fragment>
+        );
+    }
+}
+
+export default None;
